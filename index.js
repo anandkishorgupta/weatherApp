38fadc99eb5cb56e95425584e465956090eba56f
anandkishorgupta <anandgupta20588@gmail.com>
2023-07-22 08:30:57 +0545

    fixing temperature

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function renderWeatherInfo(weatherInfo) {
   countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
   desc.innerText = weatherInfo?.weather?.[0]?.description;
   weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
-  temp.innerText = (weatherInfo?.main?.temp- 273.15) + "°C";
+  temp.innerText = (weatherInfo?.main?.temp- 273.15).toFixed(2) + "°C";
   windspeed.innerText = `${weatherInfo?.wind?.speed}m/s`;
   humidity.innerText = `${weatherInfo?.main?.humidity}%`;
   cloudiness.innerText = `${weatherInfo?.clouds?.all}%`;
